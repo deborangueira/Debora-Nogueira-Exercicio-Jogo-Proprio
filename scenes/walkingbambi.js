b2ef96f2af43b25e7557c68b1365619a63bb0b73
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-03-05 22:00:00 -0300

    Folders
    structured folders sistem for differents scenes: welcome and play

---
+++ scenes/walkingbambi.js
@@ -0,0 +1,19 @@
+class walkingbambi extends Phaser.Scene {
+    constructor() {
+        super({ key: 'walkingbambi' });
+    }
+
+    preload() {
+        // Carregar recursos
+
+    }
+
+    create() {
+        // Configuração inicial da cena
+
+    }
+
+    update() {
+        // Lógica de atualização da cena
+    }
+}
