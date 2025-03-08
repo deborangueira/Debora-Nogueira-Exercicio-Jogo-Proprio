67890b9c2a1513bc83628b8d8a5434f7a96cd2c2
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-03-08 20:59:38 -0300

    add: colision, movement, hoverSound

--- scenes/jogo.js
+++ scenes/jogo.js
@@ -13,15 +13,24 @@ class jogo extends Phaser.Scene {
         });
     } 
 
+    init() {
+        this.bambi;
+        this.teclado;
+        this.ground;
+
+    }
+
+
     preload() {
         // Carregar recursos
 
         this.load.image('bg', 'Assets/bg_c1.png');
+        this.load.image('ground','Assets/ground1.png')
         this.load.image('fire', 'Assets/fire.png');
         this.load.image('flower', 'Assets/flower.png');
         this.load.image('tronco', 'Assets/tronco.png');
         this.load.image('riacho', 'Assets/Riacho.png');
-        this.load.image('personagem', 'Assets/bambi.png',{ frameWidth: 32, frameHeight: 48 });
+        this.load.image('bambi', 'Assets/bambi.png',{ frameWidth: 32, frameHeight: 48 });
     }
 
     create() {
@@ -29,16 +38,44 @@ class jogo extends Phaser.Scene {
         //BACKGROUND
         this.add.image(larguraJogo/2,alturaJogo/2,'bg');
 
-        //BAMBI
-        var bambi = this.physics.add.sprite(100,0,'personagem');
-        bambi.setColliderWorldBounds(true);
-    
+        //Chão
+        this.ground = this.physics.add.staticGroup();
+        this.ground.create(600, alturaJogo - 50, 'ground').setScale(0.8).refreshBody();
 
+        //BAMBI
+        this.bambi = this.physics.add.sprite(90, 540, 'bambi').setScale(0.5);
+        this.bambi.setCollideWorldBounds(true); // impede que ele saia da tela
+
+         // Colisão entre o bambi e o chão
+        this.physics.add.collider(this.bambi, this.ground);
+   
+        // Configurando o teclado
+        this.teclado = this.input.keyboard.createCursorKeys();
 
     }
 
     update() {
-        // Lógica de atualização da cena
+
+        // Movimenta para a esquerda
+        if (this.teclado.left.isDown) {
+            this.bambi.setVelocityX(-300);
+            this.bambi.setFlipX(true);
+        } 
+        // Movimenta para a direita
+        else if (this.teclado.right.isDown) {
+            this.bambi.setVelocityX(300);
+            this.bambi.setFlipX(false);
+        } 
+        // Parado se nenhuma tecla for pressionada
+        else {
+            this.bambi.setVelocityX(0);
+        }
+
+        // Pulo apenas se estiver no chão
+        if (this.teclado.up.isDown && this.bambi.body.touching.down) {
+            this.bambi.setVelocityY(-330);
+        }
+
     }
 }
 
--- scenes/menu.js
+++ scenes/menu.js
@@ -12,6 +12,9 @@ class menu extends Phaser.Scene {
         this.load.audio('theme',[
             'Assets/audio/soundtrack.m4a'
         ]);
+        this.load.audio('hoverSound',[
+            'Assets/audio/sd_click.m4a'
+        ]);
     }
 
     create() {
@@ -22,34 +25,43 @@ class menu extends Phaser.Scene {
 
         //MÚSICA
         const music = this.sound.add('theme');
-        //music.play();
-        //this.sound.pauseOnBlur = true;
+        music.setVolume(0.3);
 
         //BOTÕES: adicionando e ativando-os
         let botao1 = this.add.image(larguraJogo/2 +31,alturaJogo/4 + 100, 'botaojogar').setInteractive();
         botao1.on('pointerdown',() => {
-            this.scene.start('jogo')
-        });
+            this.hoverSound = this.sound.add('hoverSound');
+            this.hoverSound.play({seek: 0.006, volume: 0.3 });
+            this.scene.start('jogo');
+            music.play();
+         });
+        
 
         let botao2 = this.add.image(larguraJogo/2 + 60,alturaJogo/4 + 200, 'botaotutorial').setInteractive({ useHandCursor: true });
         botao2.on('pointerdown',() => {
+            this.hoverSound = this.sound.add('hoverSound');
+            this.hoverSound.play({seek: 0.006, volume: 0.3 });
             this.scene.start('tutorial')
         });
                 
        let botao3 = this.add.image(larguraJogo/2 + 60,alturaJogo/4 + 300, 'botaocreditos').setInteractive({ useHandCursor: true });
         botao3.on('pointerdown',() => {
+            this.hoverSound = this.sound.add('hoverSound');
+            this.hoverSound.play({seek: 0.006, volume: 0.3 });
             this.scene.start('creditos')
         });
 
          // Efeito de hover para os botões
          const addHoverEffect = (button) => {
-            button.on('pointerover', () => button.setScale(1.2));
-            button.on('pointerout', () => button.setScale(1));
+            button.on('pointerover', () => 
+                button.setScale(1.2));
+            button.on('pointerout', () => 
+                button.setScale(1));
         };
+
         addHoverEffect(botao1);
         addHoverEffect(botao2);
         addHoverEffect(botao3);
-
 
     }
     
